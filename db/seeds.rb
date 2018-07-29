6ee7d1742b8761b13decba2a79fcfb3c01998cdc
iceland101113 <r02521115@ntu.edu.tw>
2018-07-29 14:55:03 +0800

    setup seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+category_list = [
+  { name: "00_基本圖資" },
+  { name: "01_地籍圖資" },
+  { name: "02_數值地形模型(DTM)" },
+  { name: "03_休閒農業區" },
+  { name: "04_林務相關" },
+  { name: "05_農地盤查相關" },
+  { name: "06_國土利用調查相關" },
+  { name: "07_都市計畫相關" },
+  { name: "08_農田水利會相關" },
+  { name: "09_農村再生社區" },
+  { name: "10_漁業署圖資" },
+  { name: "11_農糧署相關" },
+  { name: "12_農試所相關" },
+  { name: "13_水土保持局相關" },
+  { name: "14_畜牧場相關" },
+  { name: "15_國家公園" },
+  { name: "16_學校相關" },
+  { name: "17_浪漫臺三線" },
+  { name: "18_地籍化成果" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
